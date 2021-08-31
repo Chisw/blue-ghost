24888faf967c46fb30a268836424c29c25f25bcf
Chisw <chisw@qq.com>
2021-08-31 22:58:08 +0800

    update x-plore dlg style

--- content-script/x-plore/x-plore.js
+++ content-script/x-plore/x-plore.js
@@ -1,7 +1,11 @@
 ; (function () {
 
-  const style = document.createElement('style')
-  style.innerText = window.injectStyleContent
-  document.body.appendChild(style)
+  const { host, search } = location
+
+  if (host.startsWith('192.168') && search === '?x') {
+    const style = document.createElement('style')
+    style.innerText = window.injectStyleContent
+    document.body.appendChild(style)
+  }
 
 }());
